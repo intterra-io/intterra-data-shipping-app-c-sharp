8c4db8256257d99dc1d226565a6b48f22eec50e0
Jason Posthuma <jason.posthuma@intterragroup.com>
2020-02-05 11:39:20 -0700

    removed unnecessary usings

--- DSA.Library.Core/Data/LogClient.cs
+++ DSA.Library.Core/Data/LogClient.cs
@@ -1,10 +1,7 @@
 ﻿using DSA.Lib.Core.Models;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DSA.Lib.Core.Data
 {
